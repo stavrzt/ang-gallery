64e501e82877c32d3114ef9fb7d92e519d074be1
mudryi <stavrzt@gmail.com>
2017-07-11 22:28:37 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { AlbumsService } from './services/albums.service';
+
+import { OnInit } from '@angular/core';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AlbumsService],
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+  lol;
+
+  constructor(private _albumsService: AlbumsService) {
+  }
+
+  ngOnInit() {
+    this._albumsService.getData().subscribe((data: Response) => this.lol = data.json());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { UniquePipe } from './pipes/unique.pipe';
+import { UsersComponent } from './components/users/users.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { PhotoListComponent } from './components/photo-list/photo-list.component';
+import { PhotoDetailComponent } from './components/photo-detail/photo-detail.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UniquePipe,
+        UsersComponent,
+        AlbumsComponent,
+        PhotoListComponent,
+        PhotoDetailComponent
+    ],
+    imports: [
+        BrowserModule, HttpModule
+    ],
+    providers: [UniquePipe],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
